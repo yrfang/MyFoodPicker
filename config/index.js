9064e9cc29a9a92ef752529cbb298c1e980d1ae2
Henry <wanghy917@gmail.com>
2017-10-03 15:43:33 +0800

    test

--- config/index.js
+++ config/index.js
@@ -26,7 +26,7 @@ module.exports = {
     port: 8080,
     autoOpenBrowser: true,
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/MyFoodPicker/',
+    assetsPublicPath: '/MyFoodPicker/dist/',
     proxyTable: {},
     // CSS Sourcemaps off by default because relative paths are "buggy"
     // with this option, according to the CSS-Loader README
